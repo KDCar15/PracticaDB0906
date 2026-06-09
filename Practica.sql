ba6e936185fe059a3e1836a0ab8d9a9344e0dd41
KDCar15 <caaguirre@uamv.edu.ni>
2026-06-09 08:10:15 -0600

    Tabla Empleado

--- Practica.sql
+++ Practica.sql
@@ -43,3 +43,42 @@ CREATE TCargo
 		UNIQUE(cNombreCargo)
 );
 GO
+
+CREATE TABLE TEmpleado
+(
+	nEmpleadoID INT
+		IDENTITY(1,1)
+	,nDepartamentoID INT
+	,nCargoID INT
+	
+	,nSalario DECIMAL 
+		NOT NULL
+	,cNIF NVARCHAR(15)
+		NOT NULL
+	,cNombre NVARCHAR(50)
+		NOT NULL
+	,cApellido NVARCHAR(50) 
+		NOT NULL
+	,dFechaContratacion DATE
+		DEFAULT(GETDATE())
+		
+	,dCreatedAt DATETIME
+		DEFAULT(GETDATE())
+	,dUpdatedAt
+		NULL
+	,dDeletedAt
+		NULL
+	
+	,CONSTRAINT pk_empleadoid
+		PRIMARY KEY(nEmpleadoID)
+	,CONSTRAINT fk_departamentoid
+		FOREIGN KEY(nDepartamentoID)
+		REFERENCES TDepartamento(nDepartamentoID)
+	,CONSTRAINT fk_cargoid
+		FOREIGN KEY(nCargoID)
+		REFERENCES TCargo(nCargoID)
+		
+	,CONSTRAINT ck_salario
+		CHECK(nSalario >= 300)
+);
+GO
