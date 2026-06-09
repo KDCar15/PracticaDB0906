425547d235ed153ffccf64bc6adfc43910930f73
KDCar15 <caaguirre@uamv.edu.ni>
2026-06-09 08:08:50 -0600

    Tabla Departamentos

--- Practica.sql
+++ Practica.sql
@@ -1,3 +1,24 @@
 CREATE DATABASE EmpresaSQL
 
 USE EmpresaSQL
+
+CREATE TABLE TDepartamento
+(
+	nDepartamentoID INT
+		IDENTITY(1,1)
+	,cNombreDepartamento NVARCHAR(50)
+		NOT NULL
+		
+	,dCreatedAt DATETIME
+		DEFAULT(GETDATE())
+	,dUpdatedAt 
+		NULL
+	,dDeletedAt
+		NULL
+	
+	,CONSTRAINT pk_departamento
+		PRIMARY KEY(nDepartamentoID)
+	,CONSTRAINT uq_nomdepart
+		UNIQUE(cNombreDepartamento)
+);
+GO
